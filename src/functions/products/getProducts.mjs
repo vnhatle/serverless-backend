b193be649f450f9095bebaa60e310ecc7243798c
vnhatle <lenhat.bkit@gmail.com>
2025-04-21 13:23:00 -0500

    fix pipelien

--- src/functions/products/getProducts.mjs
+++ src/functions/products/getProducts.mjs
@@ -1,42 +1,47 @@
-import db from '../../utils/db.mjs';
+import db from "../../utils/db.mjs";
 
 export const handler = async (event) => {
-  try {
-    const { queryStringParameters } = event;
-    const query = queryStringParameters?.query || '';
-    
-    // Get products from DynamoDB
-    const products = await db.scan(process.env.PRODUCT_TABLE);
-    
-    // If query parameter is provided, filter products
-    let filteredProducts = products;
-    if (query) {
-      const lowercaseQuery = query.toLowerCase();
-      filteredProducts = products.filter(
-        product => 
-          product.name.toLowerCase().includes(lowercaseQuery) || 
-          product.description.toLowerCase().includes(lowercaseQuery)
-      );
+    try {
+        const { queryStringParameters } = event;
+        const query = queryStringParameters?.query || "";
+
+        // Get products from DynamoDB
+        const products = await db.scan(process.env.PRODUCT_TABLE);
+
+        console.log("Nhat");
+
+        // If query parameter is provided, filter products
+        let filteredProducts = products;
+        if (query) {
+            const lowercaseQuery = query.toLowerCase();
+            filteredProducts = products.filter(
+                (product) =>
+                    product.name.toLowerCase().includes(lowercaseQuery) ||
+                    product.description.toLowerCase().includes(lowercaseQuery)
+            );
+        }
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify(filteredProducts),
+        };
+    } catch (error) {
+        console.error("Error fetching products:", error);
+
+        return {
+            statusCode: 500,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                message: "Error fetching products",
+                error: error.message,
+            }),
+        };
     }
-    
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify(filteredProducts)
-    };
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({ message: 'Error fetching products', error: error.message })
-    };
-  }
 };
